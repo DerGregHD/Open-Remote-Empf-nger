b2a2e451f4886efb4171ef763e0cb0d2f9bd7eff
DerGregHD <55300769+DerGregHD@users.noreply.github.com>
2022-10-09 16:24:58 +0200

    ServoStruct hinzugefügt

--- OREmain/OREmain.ino
+++ OREmain/OREmain.ino
@@ -7,8 +7,29 @@
 RF24 radio(7, 8);
 uint8_t address[][6] = {"00001"};
 
-int PWMValue = 90;
+//Struktur für Servowerte
+struct ServoData {
+  byte sD0=0;
+  byte sD1=0;
+  byte sD2=0;
+  byte sD3=0;
+  byte sD4=0;
+  byte sD5=0;
+  byte sD6=0;
+  byte sD7=0;
+  byte sD8=0;
+  byte sD9=0;
+  byte sD10=0;
+  byte sD11=0;
+  byte sD12=0;
+  byte sD13=0;
+  byte sD14=0;
+  byte sD15=0;
+};
 
+ServoData servoData;
+
+Servo servo0;
 Servo servo1;
 
 void setup() {
@@ -21,11 +42,12 @@ void setup() {
       }
   }
   radio.setPALevel(RF24_PA_LOW);
-  radio.setPayloadSize(sizeof(PWMValue));
+  radio.setPayloadSize(sizeof(servoData));
   radio.openReadingPipe(1, address[0]);
   radio.startListening();
 
-  servo1.attach(21);
+  servo0.attach(21);
+  servo1.attach(22);
 }
 
 void loop() {
@@ -34,9 +56,13 @@ void loop() {
   uint8_t pipe;
   if (radio.available(&pipe)) {              // is there a payload? get the pipe number that recieved it
     uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
-    radio.read(&PWMValue, bytes);             // fetch payload from FIFO
-    servo1.write(PWMValue);
-    Serial.println(PWMValue);
+    radio.read(&servoData, bytes);
+    Serial.print("sD0: ");
+    Serial.print(servoData.sD0);
+    Serial.print(" sD1: ");
+    Serial.println(servoData.sD1);             // fetch payload from FIFO
+    servo0.write(servoData.sD0);
+    servo1.write(servoData.sD1);
     delay(10);
   }else{
     Serial.println(F("radio is not available"));
