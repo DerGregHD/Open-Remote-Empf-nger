9d2a9a8da7cffcb5d94b48cd96bcfdd20db2ed05
DerGregHD <55300769+DerGregHD@users.noreply.github.com>
2022-10-20 12:44:02 +0200

    Update OREmain.ino

--- OREmain/OREmain.ino
+++ OREmain/OREmain.ino
@@ -12,7 +12,7 @@
 bool blink = LOW;
 
 //RF24
-RF24 radio(7, 8);
+RF24 radio(20, 17);
 uint8_t address[][6] = {"00001"};
 
 //Struktur für Servowerte
@@ -39,6 +39,14 @@ ServoData servoData;
 
 Servo servo0;
 Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+Servo servo7;
+Servo servo8;
+Servo servo9;
 
 //########## methods ##########
 
@@ -57,8 +65,16 @@ void setup() {
   radio.openReadingPipe(1, address[0]);
   radio.startListening();
 
-  servo0.attach(21);
-  servo1.attach(22);
+  servo0.attach(0);
+  servo1.attach(1);
+  servo2.attach(2);
+  servo3.attach(3);
+  servo4.attach(4);
+  servo5.attach(5);
+  servo6.attach(6);
+  servo7.attach(7);
+  servo8.attach(8);
+  servo9.attach(9);
 }
 
 //########## loop code ##########
@@ -67,7 +83,7 @@ void loop() {
   blink = !blink;
   
   uint8_t pipe;
-  if (radio.available(&pipe)) {              // is there a payload? get the pipe number that recieved it
+  //if (radio.available(&pipe)) {              // is there a payload? get the pipe number that recieved it
     uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
     radio.read(&servoData, bytes);
     Serial.print("sD0: ");
@@ -76,10 +92,26 @@ void loop() {
     Serial.println(servoData.sD1);             // fetch payload from FIFO
     servo0.write(servoData.sD0);
     servo1.write(servoData.sD1);
-    delay(20);
-  }else{
-    Serial.println(F("radio is not available"));
-    servo0.write(90);
-    servo1.write(90);
-  }
+    servo2.write(servoData.sD2);
+    servo3.write(servoData.sD3);
+    servo4.write(servoData.sD4);
+    servo5.write(servoData.sD5);
+    servo6.write(servoData.sD6);
+    servo7.write(servoData.sD7);
+    servo8.write(servoData.sD8);
+    servo9.write(servoData.sD9);
+    delay(50);
+  //}else{
+    //Serial.println(F("radio is not available"));
+    //servo0.write(90);
+    //servo1.write(90);
+    //servo2.write(90);
+    //servo3.write(90);
+    //servo4.write(90);
+    //servo5.write(90);
+    //servo6.write(90);
+    //servo7.write(90);
+    //servo8.write(90);
+    //servo9.write(90);
+  //}
 }
